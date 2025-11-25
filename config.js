583848c152cd8136593a0154d6a69b078b592b55
felipelovis <vasconceloslovison@gmail.com>
2025-11-25 02:02:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzTd5SOdLOr9bD1
 // DATAS LIMITE por fase (CORRIGIDO PARA 2026!)
 const DATAS_LIMITE = {
     'Grupo': new Date('2026-06-12T14:00:00'),
-    '16 avos': new Date('2025-11-15T12:00:00'),
+    '16 avos': new Date('2026-06-12T12:00:00'),
     'Oitavas de final': new Date('2025-11-12T12:00:00'),
     'Quartas de final': new Date('2025-11-12T16:00:00'),
     'Semifinais': new Date('2025-11-12T16:00:00'),
